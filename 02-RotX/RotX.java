6c5fcb1e29646140e315e25eefd9579649378aaf
waeeeell <waelelk053@gmail.com>
2025-09-26 16:35:55 +0200

    primer push

---
+++ 02-RotX/RotX.java
@@ -0,0 +1,22 @@
+public class RotX {
+
+    char[] minuscules = "abcdefghijklmnopqrstuvwxyzçñáàéèíìóòúùü".toCharArray();
+    char[] majuscules = "ABCDEFGHIJKLMNOPQRSTUVWXYZÇÑÁÀÉÈÍÌÓÒÚÙÜ".toCharArray();
+
+    public static void main(String[] Args) {
+
+        int desplacament;
+
+    }
+
+    public String xifraRotX(String cadena, int desplacament) {
+        String resultat = "";
+        return resultat;
+    }
+
+    public String desXifraRotX(String cadena, int desplacament) {
+        String resultat = "";
+        return resultat;
+
+    }
+}
