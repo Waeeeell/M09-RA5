be60a916f2c431ebb0b772badf991f442cad4fea
waeeeell <waelelk053@gmail.com>
2025-09-26 17:27:54 +0200

    mirando de incrementar de 0 a 39 con for

--- 02-RotX/RotX.java
+++ 02-RotX/RotX.java
@@ -5,17 +5,33 @@ public class RotX {
 
     public static void main(String[] Args) {
 
-        int desplacament;
+        int desplacament = 0; // inicio desplacamiento en 0
+        String[] cadena = {
 
+                "ABC",
+                "XZY",
+                "Hola, Mr calçot",
+                "Perdó, per tu què és?"
+
+        };
     }
 
     public String xifraRotX(String cadena, int desplacament) {
         String resultat = "";
+        int auxDesplacament = desplacament;
+
+        for (int i = 0; i <= minuscules.length; i++) { // posible for para hacer incremento de desplacamiento (de 0 a
+                                                       // 39)
+
+        }
+
         return resultat;
     }
 
     public String desXifraRotX(String cadena, int desplacament) {
         String resultat = "";
+        int incrdesPlacament = desplacament;
+
         return resultat;
 
     }
