bd01cef140bbc001e4beac2e1ace042fd33139e6
waeeeell <waelelk053@gmail.com>
2025-09-30 01:04:03 +0200

    falta forca bruta

--- 02-RotX/RotX.java
+++ 02-RotX/RotX.java
@@ -16,23 +16,85 @@ public class RotX {
         };
     }
 
-    public String xifraRotX(String cadena, int desplacament) {
+    public String xifraRot13(String cadena, int desplacament) {
         String resultat = "";
-        int auxDesplacament = desplacament;
+        // String aux = cadena; //un string auxiliar por si acaso la nescesito
 
-        for (int i = 0; i <= minuscules.length; i++) { // posible for para hacer incremento de desplacamiento (de 0 a
-                                                       // 39)
+        for (int i = 0; i < cadena.length(); i++) { // recorro cel texto que quiero cifrar
+            char lletra = cadena.charAt(i);
+            boolean trobat = false;
 
+            for (int j = 0; j < minuscules.length; j++) { // recorremos minuscular
+                if (lletra == minuscules[j]) { // si la letra esta en el array d minusculas
+                    int rota13 = (j + desplacament) % minuscules.length; // cojo la posicion de la letra j puede ser
+                                                                         // cualquier
+                    // numero y le sumo 13
+                    resultat += minuscules[rota13]; // esa suma anterior da un numero que sera la posicion d la
+                                                    // letra que toca
+                    trobat = true;
+                    break;
+                }
+            }
+
+            if (!trobat) {
+                for (int k = 0; k < majuscules.length; k++) {
+                    if (lletra == majuscules[k]) {
+                        trobat = true;
+                        int rota13 = (k + desplacament) % majuscules.length;
+                        resultat += majuscules[rota13];
+                        break;
+                    }
+                }
+            }
+
+            if (!trobat) { // si no sale en ningun array puede ser numero, espacio, signo... lo ponemos
+                resultat += lletra;
+            }
         }
 
         return resultat;
     }
 
-    public String desXifraRotX(String cadena, int desplacament) {
+    public String desxifraRot13(String cadena, int desplacament) {
         String resultat = "";
-        int incrdesPlacament = desplacament;
+
+        for (int i = 0; i < cadena.length(); i++) {
+            char lletra = cadena.charAt(i);
+            boolean trobat = false;
+
+            for (int j = 0; j < minuscules.length; j++) {
+                if (lletra == minuscules[j]) {
+                    trobat = true;
+                    int desRota13 = (j - desplacament + minuscules.length) % minuscules.length; // para que no de
+                                                                                                // negativo,
+                    // siempre sumamos la longitud del
+                    // array
+                    resultat += minuscules[desRota13];
+                    break;
+                }
+            }
+
+            if (!trobat) {
+                for (int e = 0; e < majuscules.length; e++) {
+                    if (lletra == majuscules[e]) {
+                        trobat = true;
+                        int desRota13 = (e - desplacament + majuscules.length) % majuscules.length;
+                        resultat += majuscules[desRota13];
+                        break;
+                    }
+                }
+            }
+
+            if (!trobat) { // si no esta en loa array, puede ser espacio, nuemro, algun signo. añadimos yya
+
+                resultat += lletra;
+            }
+        }
 
         return resultat;
+    }
+
+    public String forcaBrutaRotX(String cadenaXifrada) {
 
     }
 }
