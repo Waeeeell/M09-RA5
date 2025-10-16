4281d369827a6eb679e19c3b5f2ace80b6cb7abf
waeeeell <waelelk053@gmai.com>
2025-10-16 23:34:34 +0200

    canvi de clase

---
+++ 04-Polialfabetic/Polialfabetic.java
@@ -0,0 +1,10 @@
+/*
+ * 
+ */
+
+public class Polialfabetic {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ 05-AES/AES.java
@@ -0,0 +1,53 @@
+
+/*
+ * 
+ */
+import javax.crypto.*;
+import java.security.*;
+
+public class AES {
+
+    public static final String ALGORITME_XIFRAT = "AES";
+    public static final String ALGORITME_HASH = "SHA-256";
+    public static final String FORMAT_AES = "AES/CBC(PKCS5padding)";
+
+    private static final int MIDA_IV = 16;
+    private static byte[] iv = new byte[MIDA_IV];
+    private static final String CLAU = "8611mWael";
+
+    public static void main(String[] args) {
+        String msgs[] = {
+                "Lorem ipsum dicet",
+                "Hola Andrés cómo está tu cuñado",
+                "Àgora ïlla Ôtto "
+        };
+
+        for (int i = 0; i < msgs.length; i++) {
+            String msg = msgs[i];
+            byte[] bXifrats = null;
+            String desxifrat = "";
+
+            try {
+                bXifrats = xifraAES(msg, CLAU);
+                desxifrat = desxifraAES(bXifrats, CLAU);
+            } catch (Exception e) {
+                System.err.println("Error de xifrat: "
+                        + e.getLocalizedMessage());
+            }
+            System.out.println("-----------------------");
+            System.out.println("Msg: " + msg);
+            System.out.println("Enc: " + new String(bXifrats));
+            System.out.println("DEC: " + desxifrat);
+        }
+
+    }
+
+    public static byte[] xifraAES(String msg String password) throws Exception{
+
+    }
+
+    public String desXifraAES(byte[] bMsgXifrat, String password) throws Exception {
+
+    }
+
+}
